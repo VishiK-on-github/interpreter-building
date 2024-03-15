24ef4234bc0b90e4030de9cace571a793388351a
VishiK-on-github <vck281000@gmail.com>
2024-03-16 01:56:27 +0530

    if and while block + tests

--- codes/eva.js
+++ codes/eva.js
@@ -33,6 +33,23 @@ class Eva {
 			return this.eval(exp[1], env) / this.eval(exp[2], env);
 		}
 		// -------------------------------
+		// Comparators
+		if (exp[0] === ">") {
+			return this.eval(exp[1], env) > this.eval(exp[2], env);
+		}
+		if (exp[0] === "<") {
+			return this.eval(exp[1], env) < this.eval(exp[2], env);
+		}
+		if (exp[0] === ">=") {
+			return this.eval(exp[1], env) >= this.eval(exp[2], env);
+		}
+		if (exp[0] === "<=") {
+			return this.eval(exp[1], env) <= this.eval(exp[2], env);
+		}
+		if (exp[0] === "=") {
+			return this.eval(exp[1], env) === this.eval(exp[2], env);
+		}
+		// -------------------------------
 		// Variable declaration
 		if (exp[0] === "var") {
 			const [_, name, value] = exp;
@@ -48,6 +65,25 @@ class Eva {
 		// Variable access
 		if (isVariableName(exp)) {
 			return env.lookup(exp);
+		}
+		// ------------------------------
+		// If expression
+		if (exp[0] === "if") {
+			const [_tag, condition, consequent, alternate] = exp;
+			if (this.eval(condition, env)) {
+				return this.eval(consequent, env);
+			}
+			return this.eval(alternate, env);
+		}
+		// -------------------------------
+		// while expression
+		if (exp[0] === "while") {
+			const [_tag, condition, body] = exp;
+			let result;
+			while (this.eval(condition, env)) {
+				result = this.eval(body, env);
+			}
+			return result;
 		}
 		// -------------------------------
 		// Block: sequence of expressions
