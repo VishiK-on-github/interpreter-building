a58e5e3db8cdf1bbcce15cc2ba4d250ebfaed598
VishiK-on-github <vck281000@gmail.com>
2024-03-19 23:51:27 +0530

    recursion test case

--- codes/__tests__/user-defined-function-test.js
+++ codes/__tests__/user-defined-function-test.js
@@ -52,4 +52,21 @@ module.exports = (eva) => {
 	`,
 		160
 	);
+	// recursion
+	test(
+		eva,
+		`
+		(
+			begin 
+			(def factorial (x)
+				(if (= x 1)
+					1
+					(* x (factorial (- x 1)))
+				)
+			)
+			(factorial 5)
+		)
+		`,
+		120
+	);
 };
